386447e27eb247a778a1c8c07128c9b24cfa9e9b
philjchristian <phil.jchristian@gmail.com>
2021-10-19 19:29:27 -0400

    Test Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from math123 import add1234
+from person import Person
+from math import tan
+
+name = "Blah"
+name2 = "Blah2"
+blah = tan(1 / 2)
+
+
+def alex_method(name):
+    p = Person(name)
+    print(p)
+
+
+if __name__ == '__main__':
+    # TODO: fix bug
+    # TODO: implement test
+    print("pycharm is awesome")
+    print("pycharm is awesome")
+    print(add1234(1, 2))
+    alex_method('alex')
---
+++ math123.py
@@ -0,0 +1,2 @@
+def add1234(x, y):
+    return x + y
